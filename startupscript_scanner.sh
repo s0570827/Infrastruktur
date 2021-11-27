961d7e19add905c9d5b2a3b1ad4de3a25c544330
Masashi Matsuo <51855653+s0570827@users.noreply.github.com>
2021-11-27 04:16:24 +0100

    Update startupscript_scanner.sh

--- startupscript_scanner.sh
+++ startupscript_scanner.sh
@@ -8,7 +8,6 @@ wget https://raw.githubusercontent.com/s0570827/Infrastruktur/main/scan_url.sh
 
 chmod +x scan_url.sh
 
-(crontab -l; echo "0 */12 * * * /home/ubuntu/scan_url.sh > /dev/null 2>&1")|awk '!x[$0]++'|crontab -
-
+(crontab -l; echo "0 */12 * * * bash /home/ubuntu/scan_url.sh > /dev/null 2>&1")|awk '!x[$0]++'|crontab -
 
 sudo reboot
