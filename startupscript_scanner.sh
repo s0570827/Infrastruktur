38dfd9f14168f4d63f7e42b7f5df91a5467ee5e9
Masashi Matsuo <51855653+s0570827@users.noreply.github.com>
2021-11-27 03:02:22 +0100

    Update startupscript_scanner.sh

--- startupscript_scanner.sh
+++ startupscript_scanner.sh
@@ -7,8 +7,8 @@ sudo apt install -y awscli
 wget https://raw.githubusercontent.com/s0570827/Infrastruktur/main/scan_url.sh
 
 chmod +x scan_url.sh
-#0 */12 * * *
-(crontab -l; echo "*/5 * * * * /home/ubuntu/scan_url.sh")|awk '!x[$0]++'|crontab -
+
+(crontab -l; echo "0 */12 * * * /home/ubuntu/scan_url.sh > /dev/null 2>&1")|awk '!x[$0]++'|crontab -
 
 
 sudo reboot
